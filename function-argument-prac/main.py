0bb93a1f4a61f9970cd380dc191815784b9bbcd3
Ayush Rawat <154885892+ayrawdev@users.noreply.github.com>
2026-02-26 16:47:51 +0530

    main.py

---
+++ function-argument-prac/main.py
@@ -0,0 +1,6 @@
+def name(fname,mname,lname):
+  print("you are",fname,mname,lname)
+a=str(input("enter your first name:"))
+b=str(input("enter your middle name:"))
+c=str(input("enter your last name:"))
+name(a.title(),b.title(),c.title())
