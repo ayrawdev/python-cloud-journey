16ed9f6ddfaaf5925ca1d49484de6f4243d4a1e7
Ayush Rawat <154885892+ayrawdev@users.noreply.github.com>
2026-03-20 23:34:11 +0530

    error handling program

---
+++ error-handling-program/main.py
@@ -0,0 +1,24 @@
+STUDENTS_ID = {
+  21016112910: "MOHIT SINGH",
+  21016112911: "RAJ GUPTA",
+  21016112912: "ROHAN SHARMA",
+  21016112913: "ABHI TYAGI",
+  21016112914: "SOHIT SHARMA",
+  21016112915: "PRIYANSHU RAWAT",
+  21016112916: "KANISHQ YADAV",
+  21016112917: "ASHI DIXIT",
+  21016112918: "AYUSH SINGH",
+  21016112919: "KHUSHI PANDEY",
+  21016112920: "SHUBHAM SHARMA",
+}
+
+try:
+    i = int(input("Enter your student id: "))
+
+    if i in STUDENTS_ID:
+        print(f"Name: {STUDENTS_ID[i]} | Roll Number: {i}")
+    else:
+        print("Student ID not found")
+
+except ValueError:
+    print("Invalid input! Please enter a number.")
