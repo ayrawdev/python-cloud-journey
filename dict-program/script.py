4d5352a046838f1f4b39d729384f8f045ee9d7d1
Ayush Rawat <154885892+ayrawdev@users.noreply.github.com>
2026-03-20 14:27:56 +0530

    dict

---
+++ dict-program/script.py
@@ -0,0 +1,13 @@
+students = {
+    222: "naman",
+    223: "satyam",
+    224: "ayush",
+    225: "chetanya",
+    226: "sidhharath",
+    227: "sarthak",
+    228: "vishwesh"
+}
+
+print(students.keys())
+for keys in students.keys():
+    print(f"the students that are present are {students[keys]}, with roll number {keys}")
